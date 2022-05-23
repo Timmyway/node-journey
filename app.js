8c781389bcd169d5dff29865cfaebd1b9d5065ee
Tim <timmyway1990@gmail.com>
2022-05-23 14:12:01 +0300

    Add redirection to about-me route / change if condition to switch. / Create views file inside views folder

--- app.js
+++ app.js
@@ -4,28 +4,43 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
 	// Req contains lots of useful informations
 	// Res is the response we send back to the browser
-	res.setHeader('Content-Type', 'application/json');
-	if (req.url === '/') {
-		res.write('{ "name": "Tim", "job": "developer" }');
-		res.end();
+	res.setHeader('Content-Type', 'text/html');
+	let path = './views/';
+	switch (req.url) {
+		case '/':
+			path += 'index.html';
+			res.statusCode = 200;			
+			break;
+		case '/about':
+			path += 'about.html';
+			res.statusCode = 200;
+			break;
+		case '/about-me':
+			res.statusCode = 301;
+			res.setHeader('Location', 'about');
+			res.end();
+			break;
+		case '/devs':
+			fs.readFile(`${__dirname}/data.json`, 'utf-8', (err, data) => {
+				const devs = JSON.parse(data)
+				res.writeHead(200, {'Content-type': 'application/json'});
+				res.end(data);
+			})
+			break;	
+		default:
+			path += '404.html';
+			res.statusCode = 404;
+			break;
 	}
-	else if (req.url === '/about') {
-		res.end('About page');
-	}
-	else if (req.url === '/developers') {
-		fs.readFile(`${__dirname}/data.json`, 'utf-8', (err, data) => {
-			const devs = JSON.parse(data)
-			res.writeHead(200, {'Content-type': 'application/json'});
+
+	fs.readFile(path, (err, data) => {
+		if (err) {
+			console.log(err);
+			res.end();
+		} else {
 			res.end(data);
-		})
-	}
-	else {
-		res.writeHead(404, {
-			'Content-type': 'text/html',
-			'my-own-header': 'hello-world'
-		})
-		res.end('<h2>404 page not found !</h2>');
-	}
+		}
+	})
 })
 
 server.listen(3000, () => {
