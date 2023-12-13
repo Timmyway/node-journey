c34024927c218ceadd75e8917268939979743732
Tim <timmyway1990@gmail.com>
2023-12-13 19:51:43 +0300

    wip: Scaffold authentication system (signup, login, logout)

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,9 +1,49 @@
+const User = require("../Models/user");
+
+exports.signupPage = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup page'
+    });
+};
+
 exports.loginPage = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login page'
     });
 };
+
+exports.signup = (req, res, next) => {    
+    console.log('Signup');
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const passwordConfirm = req.body['password-confirm'];
+
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                // User has already an account
+                return res.redirect('/signup');
+            }
+
+            const user = new User({
+                username: username,
+                email: email,
+                password: password
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    res.redirect('/');
+}
 
 exports.login = (req, res, next) => {
     console.log(req.session);
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const authController = require('../controllers/AuthController');
 
+router.get('/signup', authController.signupPage);
 router.get('/login', authController.loginPage);
+router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
