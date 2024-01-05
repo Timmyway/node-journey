d92aad75f59a51125d0a7c3d7b32b719e78c2c0a
Tim <timmyway1990@gmail.com>
2024-01-05 20:03:43 +0300

    wip: Try to add custom validation

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,17 @@ const authController = require('../controllers/AuthController');
 
 router.get('/signup', authController.signupPage);
 router.get('/login', authController.loginPage);
-router.post('/signup', check('email').isEmail(), authController.signup);
+router.post('/signup',
+    check('email').isEmail()
+        .withMessage('Please enter a valid email')
+        .custom((value, { req }) => {
+            if (value === 'fr2devkontiki@gmail.com') {
+                throw new Error('This email address is forbidden');
+            }
+            return true;
+        }),    
+    authController.signup
+);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
